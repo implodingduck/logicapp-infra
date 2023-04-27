c64a5f04c8296ce7f06f567798b85706630be256
implodingduck <zsek320@gmail.com>
2023-04-27 09:27:00 -0500

    adding networking

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,3 +53,67 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
   tags = local.tags
 }
+
+resource "azurerm_virtual_network" "default" {
+  name                = "vnet-${local.gh_repo}-${var.environment}-${random_string.unique.result}-${local.loc_for_naming}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.4.0.0/24"]
+
+  tags = local.tags
+}
+
+resource "azurerm_subnet" "pe" {
+  name                 = "snet-privateendpoints-${local.loc_for_naming}"
+  resource_group_name  = azurerm_virtual_network.default.resource_group_name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes     = ["10.4.0.0/26"]
+
+  private_endpoint_network_policies_enabled = true
+
+}
+
+resource "azurerm_subnet" "logicapps" {
+  name                 = "snet-logicapps-${local.loc_for_naming}"
+  resource_group_name  = azurerm_virtual_network.default.resource_group_name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes     = ["10.4.0.64/26"]
+  service_endpoints = [
+    "Microsoft.Web",
+    "Microsoft.Storage"
+  ]
+  delegation {
+    name = "serverfarm-delegation"
+    service_delegation {
+      name = "Microsoft.Web/serverFarms"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/action",
+      ]
+    }
+  }
+}
+
+resource "azurerm_private_dns_zone" "blob" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_private_dns_zone" "file" {
+  name                = "privatelink.file.core.windows.net"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "blob" {
+  name                  = "pdns-blob"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.blob.name
+  virtual_network_id    = azurerm_virtual_network.default.id
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "file" {
+  name                  = "pdns-file"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.file.name
+  virtual_network_id    = azurerm_virtual_network.default.id
+}
+
