20920073c7e4ef5fb76f209580ffb8fb920c6fb5
implodingduck <zsek320@gmail.com>
2023-05-11 11:32:49 -0500

    adding sku name

--- terraform/main.tf
+++ terraform/main.tf
@@ -167,7 +167,7 @@ resource "azurerm_key_vault" "kv" {
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
-
+  sku_name                   = "standard"
 }
 
 resource "azurerm_key_vault_access_policy" "current" {
